67e6779cf1aeb3ca31a82b7541363f5c5261a80f
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-11-19 11:38:51 +0300

    Removed internal map. Now store only Properties

--- Lab5/PropertiesExplorer.java
+++ Lab5/PropertiesExplorer.java
@@ -3,27 +3,29 @@ import java.util.*;
 
 public class PropertiesExplorer {
     private Properties prop = new Properties();
-    private Map<String, String> content = new HashMap<>();
 
 
     public PropertiesExplorer(String path) throws IOException {
         prop.load(new FileInputStream(path));
-        readToMap();
     }
 
-    public Map<String, String> getContent() {
-        return content;
+    public Map getContent() {
+        return prop;
     }
 
-    public String getProperty(String key) {
-        return content.get(key);
+    public Collection<Object> values() {
+        return prop.values();
     }
 
-    public String getProperty(String key, String defaultValue) {
-        if (content.get(key) == null) {
+    public Object getProperty(String key) {
+        return prop.get(key);
+    }
+
+    public Object getProperty(String key, String defaultValue) {
+        if (prop.get(key) == null) {
             return defaultValue;
         }
-        return content.get(key);
+        return prop.get(key);
     }
 
     public void list(PrintStream out) {
@@ -36,25 +38,21 @@ public class PropertiesExplorer {
 
     public void load(InputStream inStream) throws IOException {
         prop.load(inStream);
-        readToMap();
     }
 
     public void load(Reader reader) throws IOException {
         prop.load(reader);
-        readToMap();
     }
 
     public void loadFromXML(InputStream in) throws IOException, InvalidPropertiesFormatException {
         prop.loadFromXML(in);
-        readToMap();
     }
 
     public Set<String> stringPropertyNames() {
-        return content.keySet();
+        return prop.stringPropertyNames();
     }
 
     public Object setProperty(String key, String value) {
-        content.put(key, value);
         return prop.setProperty(key, value);
     }
 
@@ -76,10 +74,5 @@ public class PropertiesExplorer {
 
     public void clear() {
         prop.clear();
-        content.clear();
-    }
-
-    private void readToMap() {
-        prop.keys().asIterator().forEachRemaining((it) -> content.put(it.toString(), prop.getProperty(it.toString())));
     }
 }
