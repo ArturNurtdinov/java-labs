634662bcadb7b355a305db9c29fa50c52ec31e0f
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-12-19 22:29:36 +0300

    Some changes in 4th lab for correct input/output in GUI interface

--- Lab4/explorers/FileExplorer.java
+++ Lab4/explorers/FileExplorer.java
@@ -1,20 +1,16 @@
 package explorers;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.InvalidPathException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FileExplorer {
     private StringBuilder currentPath;
-    private Scanner scanner = new Scanner(new InputStreamReader(System.in));
+    private Scanner scanner;
+    private PrintStream out;
     private final Map<String, Consumer<String>> functions = new HashMap<>() {{
         put("cd", FileExplorer.this::goToDirectory);
         put("create", FileExplorer.this::createFile);
@@ -26,36 +22,61 @@ public class FileExplorer {
 
     public FileExplorer() {
         currentPath = new StringBuilder("C:\\");
+        scanner = new Scanner(new InputStreamReader(System.in));
+        out = System.out;
+    }
+
+    public FileExplorer(InputStream in, PrintStream out) {
+        currentPath = new StringBuilder("C:\\");
+        scanner = new Scanner(in);
+        this.out = out;
     }
 
     public FileExplorer(String currentPath) {
         if (new File(currentPath).isDirectory()) {
             this.currentPath = new StringBuilder(currentPath);
+            scanner = new Scanner(new InputStreamReader(System.in));
+            out = System.out;
         } else {
             throw new IllegalArgumentException("Path is not a directory");
         }
     }
 
+    public void execute(String command) {
+        Scanner line = new Scanner(command.trim());
+
+        if (line.hasNext()) {
+            functions.getOrDefault(line.next(), (c) -> out.println("No such command: " + c)).accept(line.nextLine().trim());
+        }
+
+        out.println(currentPath.toString());
+    }
+
     public void start() {
-        System.out.println("1. cd <folder_name> to go to folder");
-        System.out.println("2. create <file_name>.<file_type> to create file. " +
+        out.println("1. cd <folder_name> to go to folder");
+        out.println("2. create <file_name>.<file_type> to create file. " +
                 "After that you can input line to create file with it or leave this line empty");
-        System.out.println("3. delete <file_name>.<file_type> to delete text file.");
-        System.out.println("4. append <file_name>.<file_type> to start append mode to file. After that you can input line");
-        System.out.println("5. ls <folder_name> to get list of files in folder. " +
+        out.println("3. delete <file_name>.<file_type> to delete text file.");
+        out.println("4. append <file_name>.<file_type> to start append mode to file. After that you can input line");
+        out.println("5. ls <folder_name> to get list of files in folder. " +
                 "<folder_name> can be empty, so you will get list of files in current directory");
-        System.out.println("6. read <file_name>.<file_type> to print content of file");
-        System.out.println(currentPath.toString());
+        out.println("6. read <file_name>.<file_type> to print content of file");
+        out.println(currentPath.toString());
         while (scanner.hasNext()) {
             try {
                 String command = scanner.next();
                 functions.get(command).accept(scanner.nextLine().trim());
+                out.println(currentPath.toString());
             } catch (NullPointerException ex) {
-                System.out.println("Unknown command");
+                out.println("Unknown command");
             } catch (Exception ex) {
-                System.out.println("Error: " + ex.getMessage());
+                out.println("Error: " + ex.getMessage());
             }
         }
+    }
+
+    public String getPath() {
+        return currentPath.toString();
     }
 
     public void close() {
@@ -70,22 +91,21 @@ public class FileExplorer {
             } catch (IndexOutOfBoundsException ex) {
                 currentPath = new StringBuilder("");
             }
-            System.out.println(currentPath.toString());
         } else {
             if (new File(currentPath + path).isDirectory()) {
                 currentPath.append(path).append("\\");
-                System.out.println(currentPath.toString());
             } else {
-                throw new IllegalArgumentException("Is not a directory");
+                throw new IllegalArgumentException("Is not a directory: " + currentPath + path);
             }
         }
     }
 
     private void createFile(String name) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(currentPath + name, true))) {
-            writer.println(scanner.nextLine());
+        Scanner line = new Scanner(name);
+        try (PrintWriter writer = new PrintWriter(new FileWriter(currentPath + line.next(), true))) {
+            writer.println(line.nextLine().trim());
         } catch (IOException ex) {
-            System.out.println("IO Error: " + ex.getMessage());
+            out.println("IO Error: " + ex.getMessage());
         }
     }
 
@@ -97,34 +117,33 @@ public class FileExplorer {
     }
 
     private void appendToFile(String name) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(currentPath + name, true))) {
-            writer.print(scanner.nextLine());
+        Scanner line = new Scanner(name);
+        try (PrintWriter writer = new PrintWriter(new FileWriter(currentPath + line.next(), true))) {
+            writer.print(line.nextLine());
         } catch (IOException ex) {
-            System.out.println("IO Error: " + ex.getMessage());
+            out.println("IO Error: " + ex.getMessage());
         }
     }
 
     private void showAllFiles(String path) {
-        try (Stream<Path> walk = Files.walk(Paths.get(currentPath + path))) {
-
-            List<String> result = walk.filter(Files::isRegularFile)
-                    .map(Path::toString).collect(Collectors.toList());
-
-            result.forEach(System.out::println);
-
-        } catch (IOException ex) {
-            System.out.println("IO Error: " + ex.getMessage());
+        File dir = new File(currentPath.toString() + path);
+        if (!dir.isDirectory()) {
+            throw new IllegalArgumentException("Not a directory: " + currentPath.toString() + path);
         }
+        for (File file : dir.listFiles()) {
+            out.println(file.getName());
+        }
+
     }
 
     private void printTextFile(String name) {
         try (BufferedReader reader = new BufferedReader(new FileReader(currentPath + name))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                out.println(line);
             }
         } catch (IOException ex) {
-            System.out.println("IO Error: " + ex.getMessage());
+            out.println("IO Error: " + ex.getMessage());
         }
     }
 }
